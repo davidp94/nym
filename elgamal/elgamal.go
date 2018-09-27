937c4a89664743b029c37af7037da8d4e02d809b
jstuczyn <jedrzej.stuczynski@gmail.com>
2018-09-27 12:24:42 +0100

    Updated elgamal package with license, docs and set of todos

--- elgamal/elgamal.go
+++ elgamal/elgamal.go
@@ -1,36 +1,68 @@
+// elgamal.go - ElGamal encryption scheme
+// Copyright (C) 2018  Jedrzej Stuczynski.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Package elgamal provides primitives required by the ElGamal encryption scheme.
+// It's based on Python's implementation: https://github.com/asonnino/coconut/blob/master/coconut/utils.py.
 package elgamal
 
 import (
 	"github.com/jstuczyn/CoconutGo/bpgroup"
-	"github.com/milagro-crypto/amcl/version3/go/amcl/BLS381"
+	// The named import is used to be able to easily update curve being used
+	Curve "github.com/milagro-crypto/amcl/version3/go/amcl/BLS381"
 )
 
-// todo: rename field names?
+// todo: rename to just 'Encryption' since it is already in elgamal package
+// todo: create types for public and private keys and adjust arguments accordingly (look https://godoc.org/golang.org/x/crypto/openpgp/elgamal)
+// todo: rather than pass entire BpGroup object, pass just rng gen, like the above implementation
+// todo: rename A and B in Encryption to c1 and c2 as is usually used in literature
+// todo: make A and B (or c1, c2) private and introduce getters
+// todo: possibly alternative version of Decrypt to return actual m rather than h^m
+
+// ElGamalEncryption are the two points on the G1 curve
+// that represent encryption of message in form of h^m
 type ElGamalEncryption struct {
-	A *BLS381.ECP
-	B *BLS381.ECP
+	A *Curve.ECP
+	B *Curve.ECP
 }
 
-func Keygen(G *bpgroup.BpGroup) (*BLS381.BIG, *BLS381.ECP) {
-	d := BLS381.Randomnum(G.Ord, G.Rng)
-	gamma := BLS381.G1mul(G.Gen1, d)
+// Keygen generates private and public keys required for ElGamal encryption scheme
+func Keygen(G *bpgroup.BpGroup) (*Curve.BIG, *Curve.ECP) {
+	d := Curve.Randomnum(G.Ord, G.Rng)
+	gamma := Curve.G1mul(G.Gen1, d)
 	return d, gamma
 }
 
-// encrypts message in the form of h^m, where h is an element of G1
-func Encrypt(G *bpgroup.BpGroup, gamma *BLS381.ECP, m *BLS381.BIG, h *BLS381.ECP) (*ElGamalEncryption, *BLS381.BIG) {
-	k := BLS381.Randomnum(G.Ord, G.Rng)
-	a := BLS381.G1mul(G.Gen1, k)
-	b := BLS381.G1mul(gamma, k) // b = (k * gamma)
-	b.Add(BLS381.G1mul(h, m))   // b = (k * gamma) + (m * h)
+// Encrypt encrypts the given message in the form of h^m,
+// where h is a point on the G1 curve using the given public key.
+// The random k is returned alongside the encryption
+// as it is required by the Coconut Scheme to create proofs of knowledge.
+func Encrypt(G *bpgroup.BpGroup, gamma *Curve.ECP, m *Curve.BIG, h *Curve.ECP) (*ElGamalEncryption, *Curve.BIG) {
+	k := Curve.Randomnum(G.Ord, G.Rng)
+	a := Curve.G1mul(G.Gen1, k)
+	b := Curve.G1mul(gamma, k) // b = (k * gamma)
+	b.Add(Curve.G1mul(h, m))   // b = (k * gamma) + (m * h)
 
 	return &ElGamalEncryption{a, b}, k
 }
 
-// returns decrypted message h^m
-func Decrypt(G *bpgroup.BpGroup, d *BLS381.BIG, enc *ElGamalEncryption) *BLS381.ECP {
-	dec := BLS381.NewECP()
+// Decrypt takes the ElGamal encryption of a message and returns a point on the G1 curve
+// that represents original h^m.
+func Decrypt(G *bpgroup.BpGroup, d *Curve.BIG, enc *ElGamalEncryption) *Curve.ECP {
+	dec := Curve.NewECP()
 	dec.Copy(enc.B)
-	dec.Sub(BLS381.G1mul(enc.A, d))
+	dec.Sub(Curve.G1mul(enc.A, d))
 	return dec
 }
--- elgamal/elgamal_test.go
+++ elgamal/elgamal_test.go
@@ -1,3 +1,19 @@
+// elgamal_test.go - ElGamal encryption scheme tests
+// Copyright (C) 2018  Jedrzej Stuczynski.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package elgamal
 
 import (
@@ -6,30 +22,30 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/jstuczyn/CoconutGo/bpgroup"
-	"github.com/milagro-crypto/amcl/version3/go/amcl/BLS381"
+	Curve "github.com/milagro-crypto/amcl/version3/go/amcl/BLS381"
 )
 
 func TestElGamalKeygen(t *testing.T) {
 	G := bpgroup.New()
 	d, gamma := Keygen(G)
 
-	assert.True(t, gamma.Equals(BLS381.G1mul(G.Gen1, d)), "Gamma should be equal to g1 * d")
+	assert.True(t, gamma.Equals(Curve.G1mul(G.Gen1, d)), "Gamma should be equal to g1 * d")
 }
 
 func TestElGamalEncryption(t *testing.T) {
 	G := bpgroup.New()
 	_, gamma := Keygen(G)
 
-	t1 := BLS381.Randomnum(G.Ord, G.Rng)
-	h := BLS381.G1mul(G.Gen1, t1) // random h
-	m := BLS381.Randomnum(G.Ord, G.Rng)
+	t1 := Curve.Randomnum(G.Ord, G.Rng)
+	h := Curve.G1mul(G.Gen1, t1) // random h
+	m := Curve.Randomnum(G.Ord, G.Rng)
 
 	enc, k := Encrypt(G, gamma, m, h)
 
-	assert.True(t, enc.A.Equals(BLS381.G1mul(G.Gen1, k)), "a should be equal to g1^k")
+	assert.True(t, enc.A.Equals(Curve.G1mul(G.Gen1, k)), "a should be equal to g1^k")
 
-	tmp := BLS381.G1mul(gamma, k) // b = (k * gamma)
-	tmp.Add(BLS381.G1mul(h, m))   // b = (k * gamma) + (m * h)
+	tmp := Curve.G1mul(gamma, k) // b = (k * gamma)
+	tmp.Add(Curve.G1mul(h, m))   // b = (k * gamma) + (m * h)
 
 	assert.True(t, enc.B.Equals(tmp), "b should be equal to (k * gamma) + (m * h)")
 }
@@ -38,10 +54,10 @@ func TestElGamalDecryption(t *testing.T) {
 	G := bpgroup.New()
 	d, gamma := Keygen(G)
 
-	t1 := BLS381.Randomnum(G.Ord, G.Rng)
-	h := BLS381.G1mul(G.Gen1, t1) // random h
-	m := BLS381.Randomnum(G.Ord, G.Rng)
-	hm := BLS381.G1mul(h, m)
+	t1 := Curve.Randomnum(G.Ord, G.Rng)
+	h := Curve.G1mul(G.Gen1, t1) // random h
+	m := Curve.Randomnum(G.Ord, G.Rng)
+	hm := Curve.G1mul(h, m)
 
 	enc, _ := Encrypt(G, gamma, m, h)
 	dec := Decrypt(G, d, enc)
