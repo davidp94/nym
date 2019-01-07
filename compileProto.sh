59e3bdf6b04de3e8902d6ba214864e115976d55d
jstuczyn <andrew@nymtoken.ch>
2019-01-07 12:12:16 +0000

    Changes to compileProto + go generate instructions. Fixes jstuczyn/CoconutGo#21

--- compileProto.sh
+++ compileProto.sh
@@ -1,23 +1,16 @@
 #!/usr/bin/env bash
 
 # elgamal
-
-protoc -I=/home/jedrzej/go/src/0xacab.org/jstuczyn/CoconutGo --go_out=/home/jedrzej/go/src/ /home/jedrzej/go/src/0xacab.org/jstuczyn/CoconutGo/crypto/elgamal/proto/types.proto 
-
+protoc --go_out=../../.. ./crypto/elgamal/proto/types.proto 
 
 # coconut
-
-protoc -I=/home/jedrzej/go/src/0xacab.org/jstuczyn/CoconutGo --go_out=/home/jedrzej/go/src/ /home/jedrzej/go/src/0xacab.org/jstuczyn/CoconutGo/crypto/coconut/scheme/proto/types.proto 
-
-# have to move file manually as for for some reason proto expects target dir to be the same as suffix of go import path
-# mv /home/jedrzej/go/src/0xacab.org/jstuczyn/CoconutGo/crypto/coconut/types.pb.go /home/jedrzej/go/src/0xacab.org/jstuczyn/CoconutGo/crypto/coconut/scheme/types.pb.go
-
-sed -i -e 's/package scheme/package coconut/g' /home/jedrzej/go/src/0xacab.org/jstuczyn/CoconutGo/crypto/coconut/scheme/types.pb.go
+# protoc insists on assigning package name being the same as the directory it is put in.
+# Currently I do not know how to override it so sed is used as workaround.
+protoc --go_out=../../.. ./crypto/coconut/scheme/proto/types.proto 
+sed -i -e 's/package scheme/package coconut/g' ./crypto/coconut/scheme/types.pb.go
 
 # commands
-
-protoc -I=/home/jedrzej/go/src/0xacab.org/jstuczyn/CoconutGo --go_out=/home/jedrzej/go/src/ /home/jedrzej/go/src/0xacab.org/jstuczyn/CoconutGo/server/commands/proto/types.proto
+protoc --go_out=../../.. ./server/commands/proto/types.proto
 
 # grpc
-
-protoc -I=/home/jedrzej/go/src/0xacab.org/jstuczyn/CoconutGo /home/jedrzej/go/src/0xacab.org/jstuczyn/CoconutGo/server/comm/grpc/proto/services.proto --go_out=plugins=grpc:/home/jedrzej/go/src
+protoc ./server/comm/grpc/proto/services.proto --go_out=plugins=grpc:../../..
