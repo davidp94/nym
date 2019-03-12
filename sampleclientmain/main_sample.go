09295d85f5ff052a55dae5921e2bf8b267dec09a
jstuczyn <andrew@nymtoken.ch>
2019-03-12 12:10:57 +0000

    Cleanup client sample main

--- sampleclientmain/main_sample.go
+++ sampleclientmain/main_sample.go
@@ -25,9 +25,11 @@ import (
 
 	cclient "0xacab.org/jstuczyn/CoconutGo/client"
 	"0xacab.org/jstuczyn/CoconutGo/client/config"
+	"0xacab.org/jstuczyn/CoconutGo/constants"
 	"0xacab.org/jstuczyn/CoconutGo/crypto/bpgroup"
 	"0xacab.org/jstuczyn/CoconutGo/crypto/coconut/scheme"
 	"0xacab.org/jstuczyn/CoconutGo/logger"
+	"0xacab.org/jstuczyn/CoconutGo/nym/token"
 	"0xacab.org/jstuczyn/CoconutGo/tendermint/account"
 	tmclient "0xacab.org/jstuczyn/CoconutGo/tendermint/client"
 	"0xacab.org/jstuczyn/CoconutGo/tendermint/nymabci/code"
@@ -56,8 +58,6 @@ func getRandomAttributes(G *bpgroup.BpGroup, n int) []*Curve.BIG {
 	return attrs
 }
 
-// TODO: cleanup....
-
 // nolint: gosec, lll, errcheck
 func main() {
 	cfgFile := flag.String("f", "config.toml", "Path to the server config file.")
@@ -65,21 +65,8 @@ func main() {
 
 	syscall.Umask(0077)
 
-	cfg, err := config.LoadFile(*cfgFile)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to load config file '%v': %v\n", *cfgFile, err)
-		os.Exit(-1)
-	}
-
 	haltCh := make(chan os.Signal)
 	signal.Notify(haltCh, os.Interrupt, syscall.SIGTERM)
-
-	// Start up the coconut client.
-	cc, err := cclient.New(cfg)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to spawn client instance: %v\n", err)
-		os.Exit(-1)
-	}
 
 	go func() {
 		for {
@@ -89,12 +76,71 @@ func main() {
 		}
 	}()
 
-	_ = cc
+	cfg, err := config.LoadFile(*cfgFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to load config file '%v': %v\n", *cfgFile, err)
+		os.Exit(-1)
+	}
+
+	// Start up the coconut client.
+	cc, err := cclient.New(cfg)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to spawn client instance: %v\n", err)
+		os.Exit(-1)
+	}
+
+	// IAInteractions(cc)
+	blockchainInteractions(cc)
+}
+
+func IAInteractions(cc *cclient.Client) {
+	useGRPC := false
 
 	params, _ := coconut.Setup(5)
 	G := params.G
 	pubM := getRandomAttributes(G, 3)
 	privM := getRandomAttributes(G, 2)
+	// all possible interactions with the IAs/SPs
+
+	if useGRPC {
+		sigGrpc, _ := cc.SignAttributesGrpc(pubM)
+		sigBlindGrpc, _ := cc.BlindSignAttributesGrpc(pubM, privM)
+		vkGrpc, _ := cc.GetAggregateVerificationKeyGrpc()
+
+		isValidGrpc, _ := cc.SendCredentialsForVerificationGrpc(pubM, sigGrpc, providerAddressGrpc)
+		isValidBlind1Grpc, _ := cc.SendCredentialsForBlindVerificationGrpc(pubM, privM, sigBlindGrpc, providerAddressGrpc, nil)
+		isValidBlind2Grpc, _ := cc.SendCredentialsForBlindVerificationGrpc(pubM, privM, sigBlindGrpc, providerAddressGrpc, vkGrpc)
+		isValidBlind3Grpc, _ := cc.SendCredentialsForVerificationGrpc(append(privM, pubM...), sigBlindGrpc, providerAddressGrpc)
+
+		fmt.Println("Is validGrpc:", isValidGrpc)
+		fmt.Println("Is valid localGrpc:", coconut.Verify(params, vkGrpc, pubM, sigGrpc))
+
+		fmt.Println("Is validBlind1Grpc:", isValidBlind1Grpc)
+		fmt.Println("Is validBlind2Grpc:", isValidBlind2Grpc)
+		fmt.Println("Is validBlind3Grpc:", isValidBlind3Grpc)
+	} else {
+		sig, _ := cc.SignAttributes(pubM)
+		sigBlind, _ := cc.BlindSignAttributes(pubM, privM)
+		vk, _ := cc.GetAggregateVerificationKey()
+
+		isValid, _ := cc.SendCredentialsForVerification(pubM, sig, providerAddress)
+		isValidBlind1, _ := cc.SendCredentialsForBlindVerification(pubM, privM, sigBlind, providerAddress, nil)
+		isValidBlind2, _ := cc.SendCredentialsForBlindVerification(pubM, privM, sigBlind, providerAddress, vk)
+		isValidBlind3, _ := cc.SendCredentialsForVerification(append(privM, pubM...), sigBlind, providerAddress)
+
+		fmt.Println("Is valid", isValid)
+		fmt.Println("Is valid local:", coconut.Verify(params, vk, pubM, sig))
+
+		fmt.Println("Is validBlind1:", isValidBlind1)
+		fmt.Println("Is validBlind2:", isValidBlind2)
+		fmt.Println("Is validBlind3:", isValidBlind3)
+	}
+
+}
+
+func blockchainInteractions(cc *cclient.Client) {
+	// decides whether we should wait for tx to be included in a block or just to pass CheckTx
+	waitForCommit := true
 
 	// create new account
 	acc := account.NewAccount()
@@ -127,145 +173,75 @@ func main() {
 		panic(fmt.Sprintf("Failed to create a tmclient: %v", err))
 	}
 
-	_ = tmclient
-	_ = cc
-	_ = newAccReq
-	_ = transferReq
+	if waitForCommit {
+		// send the requests:
+		// new acc
+		res, err := tmclient.Broadcast(newAccReq)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Created new account. Code: %v, additional data: %v\n", code.ToString(res.DeliverTx.Code), string(res.DeliverTx.Data))
+		// add some funds
+		res, err = tmclient.Broadcast(transferReq)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Transferred funds from debug to new account. Code: %v, additional data: %v\n", code.ToString(res.DeliverTx.Code), string(res.DeliverTx.Data))
+	} else {
+		// send the requests:
+		// new acc
+		res, err := tmclient.SendSync(newAccReq)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Created new account. Code: %v, additional data: %v\n", code.ToString(res.Code), string(res.Data))
+		// add some funds
 
-	// sendToHoldingReqParam := transaction.TransferToHoldingReqParams{
-	// 	ID:              ID,
-	// 	PrivateKey:      IAAcc.PrivateKey,
-	// 	ClientPublicKey: acc.PublicKey,
-	// 	Amount:          value,
-	// 	Commitment:      cm,
-	// 	ClientSig:       clientSig,
-	// }
+		res, err = tmclient.SendSync(transferReq)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Transferred funds from debug to new account. Code: %v, additional data: %v\n", code.ToString(res.Code), string(res.Data))
+	}
 
-	// sendToHoldingReq, err := transaction.CreateNewTransferToHoldingRequest(sendToHoldingReqParam)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	params, _ := coconut.Setup(5)
 
-	// // create client to interact with the abci
+	// generate token
+	value := int32(1000)
+	seq := Curve.Randomnum(params.P(), params.G.Rng())
+	privateKey := Curve.Randomnum(params.P(), params.G.Rng())
 
-	// send the requests:
-	// new acc
+	token := token.New(seq, privateKey, value)
+	pubM, privM := token.GetPublicAndPrivateSlices()
 
-	res, err := tmclient.SendSync(newAccReq)
+	// get credential
+	sig, _ := cc.BlindSignAttributes(pubM, privM)
+
+	// get aggregate vk needed for show protocol
+	avk, _ := cc.GetAggregateVerificationKey()
+
+	// generate random merchant (abci is set to create new accounts for new merchants)
+	merchantAddrEC := Curve.G1mul(params.G1(), Curve.Randomnum(params.P(), params.G.Rng()))
+	merchantAddr := make([]byte, constants.ECPLen)
+	merchantAddrEC.ToBytes(merchantAddr, true)
+
+	reqT, err := transaction.CreateNewDepositCoconutCredentialRequest(params, avk, sig, token, merchantAddr)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Created new account. Code: %v, additional data: %v\n", code.ToString(res.Code), string(res.Data))
-	// add some funds
 
-	// malform the request to cause it to fail checktx
-	transferReq[42] ^= byte(0x01) // TODO: move that into a test file...
-
-	res, err = tmclient.SendSync(transferReq)
-	if err != nil {
-		panic(err)
+	if waitForCommit {
+		res, err := tmclient.Broadcast(reqT)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Deposited Credential. Code: %v, additional data: %v\n", code.ToString(res.DeliverTx.Code), res.DeliverTx.Data)
+	} else {
+		res, err := tmclient.SendSync(reqT)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Deposited Credential. Code: %v, additional data: %v\n", code.ToString(res.Code), res.Data)
 	}
-	fmt.Printf("Transferred funds from debug to new account. Code: %v, additional data: %v\n", code.ToString(res.Code), string(res.Data))
-	// // transfer some to holding
-	// res, err = tmclient.SendAsync(sendToHoldingReq)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf("Transferred funds from new account to holding. Code: %v, additional data: %v\n", code.ToString(res.Code), string(res.Data))
 
-	// send the requests:
-	// new acc
-	// res, err := tmclient.Broadcast(newAccReq)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf("Created new account. Code: %v, additional data: %v\n", code.ToString(res.DeliverTx.Code), string(res.DeliverTx.Data))
-	// // add some funds
-	// res, err = tmclient.Broadcast(transferReq)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf("Transferred funds from debug to new account. Code: %v, additional data: %v\n", code.ToString(res.DeliverTx.Code), string(res.DeliverTx.Data))
-	// // transfer some to holding
-	// res, err = tmclient.Broadcast(sendToHoldingReq)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf("Transferred funds from new account to holding. Code: %v, additional data: %v\n", code.ToString(res.DeliverTx.Code), string(res.DeliverTx.Data))
-
-	// // generate token
-	// value := int32(1000)
-	// seq := Curve.Randomnum(params.P(), G.Rng())
-	// privateKey := Curve.Randomnum(params.P(), G.Rng())
-
-	// token := token.New(seq, privateKey, value)
-	// pubM, privM := token.GetPublicAndPrivateSlices()
-
-	// // get credential
-	// sig, _ := cc.BlindSignAttributes(pubM, privM)
-
-	// // get aggregate vk needed for show protocol
-	// avk, _ := cc.GetAggregateVerificationKey()
-
-	// // generate random merchant (abci is set to create new accounts for new merchants)
-	// merchantAddrEC := Curve.G1mul(params.G1(), Curve.Randomnum(params.P(), params.G.Rng()))
-	// merchantAddr := make([]byte, constants.ECPLen)
-	// merchantAddrEC.ToBytes(merchantAddr, true)
-
-	// tendermint-abci client
-	// log, err := logger.New("", "DEBUG", false)
-	// if err != nil {
-	// 	panic(fmt.Sprintf("Failed to create a logger: %v", err))
-	// }
-
-	// tmclient, err := tmclient.New("tcp://0.0.0.0:46667", log)
-	// if err != nil {
-	// 	panic(fmt.Sprintf("Failed to create a tmclient: %v", err))
-	// }
-
-	// reqT, err := transaction.CreateNewDepositCoconutCredentialRequest(params, avk, sig, token, merchantAddr)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// res, _ := tmclient.Broadcast(reqT)
-	// fmt.Printf("Deposited Credential. Code: %v, additional data: %v\n", code.ToString(res.DeliverTx.Code), res.DeliverTx.Data)
-
-	// all possible interactions with the IAs/SPs
-	//
-	// if cfg.Client.UseGRPC {
-	// 	sigGrpc, _ := cc.SignAttributesGrpc(pubM)
-	// 	sigBlindGrpc, _ := cc.BlindSignAttributesGrpc(pubM, privM)
-	// 	vkGrpc, _ := cc.GetAggregateVerificationKeyGrpc()
-
-	// 	isValidGrpc, _ := cc.SendCredentialsForVerificationGrpc(pubM, sigGrpc, providerAddressGrpc)
-	// 	isValidBlind1Grpc, _ := cc.SendCredentialsForBlindVerificationGrpc(pubM, privM, sigBlindGrpc, providerAddressGrpc, nil)
-	// 	isValidBlind2Grpc, _ := cc.SendCredentialsForBlindVerificationGrpc(pubM, privM, sigBlindGrpc, providerAddressGrpc, vkGrpc)
-	// 	isValidBlind3Grpc, _ := cc.SendCredentialsForVerificationGrpc(append(privM, pubM...), sigBlindGrpc, providerAddressGrpc)
-
-	// 	fmt.Println("Is validGrpc: ", isValidGrpc)
-	// 	fmt.Println("Is valid localGrpc:", coconut.Verify(params, vkGrpc, pubM, sigGrpc))
-
-	// 	fmt.Println("Is validBlind1Grpc:", isValidBlind1Grpc)
-	// 	fmt.Println("Is validBlind2Grpc:", isValidBlind2Grpc)
-	// 	fmt.Println("Is validBlind3Grpc:", isValidBlind3Grpc)
-	// } else {
-	// 	sig, _ := cc.SignAttributes(pubM)
-	// 	sigBlind, _ := cc.BlindSignAttributes(pubM, privM)
-	// 	vk, _ := cc.GetAggregateVerificationKey()
-
-	// 	isValid, _ := cc.SendCredentialsForVerification(pubM, sig, providerAddress)
-	// 	isValidBlind1, _ := cc.SendCredentialsForBlindVerification(pubM, privM, sigBlind, providerAddress, nil)
-	// 	isValidBlind2, _ := cc.SendCredentialsForBlindVerification(pubM, privM, sigBlind, providerAddress, vk)
-	// 	isValidBlind3, _ := cc.SendCredentialsForVerification(append(privM, pubM...), sigBlind, providerAddress)
-
-	// 	fmt.Println("Is valid ", isValid)
-	// 	fmt.Println("Is valid local: ", coconut.Verify(params, vk, pubM, sig))
-
-	// 	fmt.Println("Is validBlind1:", isValidBlind1)
-	// 	fmt.Println("Is validBlind2:", isValidBlind2)
-	// 	fmt.Println("Is validBlind3:", isValidBlind3)
-	// }
-	_ = privM
-	_ = pubM
 }
