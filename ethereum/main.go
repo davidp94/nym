60077fb3aa960ea84e1bc0f5ac56664dee262748
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2019-05-24 17:36:50 +0100

    Renaming variables to match latest spec.

--- ethereum/main.go
+++ ethereum/main.go
@@ -16,7 +16,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	holdingContract := common.HexToAddress("0xd6A548f60FB6F98fB29e6226DE1405c20DbbCF52")
+	pipeContract := common.HexToAddress("0xd6A548f60FB6F98fB29e6226DE1405c20DbbCF52")
 	nymContract := common.HexToAddress("0xE80025228D5448A55B995c829B89567ECE5203d3")
 
 	log, err := logger.New("", "DEBUG", false)
@@ -25,13 +25,13 @@ func main() {
 	}
 
 	cfg := client.NewConfig(privateKey,
-		[]string{"https://ropsten.infura.io/v3/5607a6494adb4ad4be814ec20f46ec5b"}, nymContract, holdingContract, log)
+		[]string{"https://ropsten.infura.io/v3/5607a6494adb4ad4be814ec20f46ec5b"}, nymContract, pipeContract, log)
 	c, err := client.New(cfg)
 	if err != nil {
 		panic(err)
 	}
 
-	if err := c.TransferERC20Tokens(context.TODO(), 1, nymContract, holdingContract); err != nil {
+	if err := c.TransferERC20Tokens(context.TODO(), 1, nymContract, pipeContract); err != nil {
 		panic(err)
 	}
 }
